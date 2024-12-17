281ae55253ad6f051b73b43a2a61693d7e5c6d75
Milton <sandovalmonasteriom@gmail.com>
2024-12-16 22:49:37 -0400

    Agregar archivos de proyecto.

---
+++ Explicacion de la POO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Explicacion_de_la_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
